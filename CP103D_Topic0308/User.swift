9a1c30e6066ccb24d1e7499a02169ff1b668c141
k86095 <k86095@gmail.com>
2019-03-15 16:57:38 +0800

    註冊

--- CP103D_Topic0308/User.swift
+++ CP103D_Topic0308/User.swift
@@ -1,13 +1,24 @@
 class User: Codable {
-    var id = -1
+    var id:Int?
     var username = ""
     var password = ""
     var name = ""
     var phone = -1
     var email = ""
+    var sex = -1
     
     init(_ username: String, _ password: String) {
         self.username = username
         self.password = password
+    }
+    
+    init(username: String, password: String, name: String, phone:Int, email:String , sex: Int) {
+        self.username = username
+        self.password = password
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.sex = sex
+        
     }
 }
--- CP103D_Topic0308/signupVC.swift
+++ CP103D_Topic0308/signupVC.swift
@@ -9,25 +9,98 @@
 import UIKit
 
 class signupVC: UIViewController {
-
+    
+    let url_server = URL(string: common_url + "UserServlet")
+    
+    @IBOutlet weak var tfName: UITextField!
+    
+    @IBOutlet weak var tfUserName: UITextField!
+    
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    
+    @IBOutlet weak var tfPhone: UITextField!
+    
+    @IBOutlet weak var scSex: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func registerClick(_ sender: Any) {
+        let username = tfUserName.text == nil ? "" : tfUserName.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = tfPassword.text == nil ? "" : tfPassword.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let name = tfName.text == nil ? "" : tfName.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let phone = tfPhone.text == nil ? "" : tfPhone.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let email = tfEmail.text == nil ? "" : tfEmail.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if username!.isEmpty || password!.isEmpty || name!.isEmpty || phone!.isEmpty || email!.isEmpty {
+            return
+        }
+        
+        let cellphone = Int(phone!)
+        let user = User(username: username!, password: password!, name: name!, phone: cellphone!, email: email!, sex: scSex.selectedSegmentIndex)
+        var requestParam = [String: String]()
+        requestParam["action"] = "insert"
+        requestParam["user"] = try! String(data: JSONEncoder().encode(user), encoding: .utf8)
+        executeTask(url_server!, requestParam) { (data, response, error) in
+            if error == nil {
+                if data != nil {
+                    if let result = String(data: data!, encoding: .utf8) {
+                        if let count = Int(result) {
+                            DispatchQueue.main.async {
+                                if count != 0 {
+                                    self.showCorrectAlert()
+                                    self.navigationController?.popViewController(animated: true)
+                                } else {
+                                    self.showErrorAlert()
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
     @IBAction func doneItem(_ sender: Any) {
+        
+        
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showErrorAlert(){
+        let errorAlert = UIAlertController(title: "Register Fail!", message: "Please check your informatiom is correct format.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        errorAlert.addAction(okAction)
+        present(errorAlert, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    func showCorrectAlert(){
+        let correctAlert = UIAlertController(title: "Register Success!", message: "Now you can login and enjoy shopping.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Great!", style: .default) { (_) in
+            self.navigationController?.popViewController(animated: true)
+        }
+        correctAlert.addAction(okAction)
+        present(correctAlert, animated: true, completion: nil)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
