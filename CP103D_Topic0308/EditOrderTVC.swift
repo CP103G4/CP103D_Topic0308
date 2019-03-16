8a1eaaa264781cd6096a0e2b10df38a091021096
k86095 <k86095@gmail.com>
2019-03-16 16:03:26 +0800

    客製化ManagerOrderCell

--- CP103D_Topic0308/EditOrderTVC.swift
+++ CP103D_Topic0308/EditOrderTVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EditOrderTVC: UITableViewController {
     
+    var order:Order?
+    
     var completionHandler:((Order) -> Void)?
 
     override func viewDidLoad() {
--- CP103D_Topic0308/ManagerOrderTVC.swift
+++ CP103D_Topic0308/ManagerOrderTVC.swift
@@ -42,14 +42,44 @@ class ManagerOrderTVC: UITableViewController {
         return orders.count
     }
     
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let edit = UITableViewRowAction(style: .default, title: "Edit", handler: { (action, indexPath) in
+            let editOrderTVC = self.storyboard?.instantiateViewController(withIdentifier: "editOrderTVC") as! EditOrderTVC
+            let order = self.orders[indexPath.row]
+            editOrderTVC.order = order
+            self.navigationController?.pushViewController(editOrderTVC, animated: true)
+        })
+        edit.backgroundColor = UIColor.lightGray
+        
+        return [edit]
+        
+    }
+    
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ordersCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ordersCell", for: indexPath) as! ManagerOrderCell
         let order = orders[indexPath.row]
-//        print(order.id?.description)
-        cell.textLabel?.text = order.id?.description
+        
+        cell.lbOrderId.text = order.id?.description
+        cell.lbOrderDate.text = order.date?.description
+        cell.lbOrderStatus.text = statusDescription(stayusCode: order.status!
+        )
+        cell.lbOrderTotalPrice.text = order.totalPrice?.description
         
         return cell
+    }
+    
+    func statusDescription(stayusCode:Int) -> (String) {
+        if stayusCode == 0 {
+            return "未出貨"
+        } else if stayusCode == 1 {
+            return "已出貨"
+        } else if stayusCode == 2 {
+            return "已退貨"
+        } else {
+            return "已取消"
+        }
     }
     
     /*
@@ -61,17 +91,17 @@ class ManagerOrderTVC: UITableViewController {
      */
     
     
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         orders.remove(at: indexPath.row)
         tableView.reloadData()
-     }
+    }
     
-//    // Override to support conditional editing of the table view.
-//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        // Return false if you do not want the specified item to be editable.
-//        return true
-//    }
+    //    // Override to support conditional editing of the table view.
+    //    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    //        // Return false if you do not want the specified item to be editable.
+    //        return true
+    //    }
     
     
     /*
