4a387cfcbad1589c0b18ccd6bb91f501f647a7b4
g87736954 <g87736954@gmail.com.tw>
2019-03-22 10:18:46 +0800

    0322 G87 增加orderdetail程式

--- CP103D_Topic0308/ConsumerOrder/G87orderdetailViewController.swift
+++ CP103D_Topic0308/ConsumerOrder/G87orderdetailViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class G87orderdetailViewController: UIViewController {
+class G87orderdetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     var order: Order?
-    
+    var goods = [Good]()
+    let url_server = URL(string: common_url + "GoodsServlet")
     @IBOutlet weak var Status: UILabel!
     @IBOutlet weak var totalprice: UILabel!
+    
+    @IBOutlet weak var orderdetailtableview: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableViewAddRefreshControl()
+        showAllOrders()
         // Do any additional setup after loading the view.
         if let order = order {
             Status.text = statusDescription(stayusCode: order.status!
@@ -37,6 +43,76 @@ class G87orderdetailViewController: UIViewController {
             return "已取消"
         }
     }
+    
+    
+    func tableViewAddRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(showAllOrders), for: .valueChanged)
+        self.orderdetailtableview.refreshControl = refreshControl
+    }
+    
+    @objc func showAllOrders(){
+        let requestParam = ["action" : "getAll"]
+        executeTask(url_server!, requestParam) { (data, response, error) in
+            
+            let decoder = JSONDecoder()
+            // JSON含有日期時間，解析必須指定日期時間格式
+            let format = DateFormatter()
+            format.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            decoder.dateDecodingStrategy = .formatted(format)
+            
+            if error == nil {
+                if data != nil {
+                    print("input: \(String(data: data!, encoding: .utf8)!)")
+                    
+                    if let result = try? decoder.decode([Good].self, from: data!) {
+                        self.goods = result
+                        DispatchQueue.main.async {
+                            if let control = self.orderdetailtableview.refreshControl {
+                                if control.isRefreshing {
+                                    // 停止下拉更新動作
+                                    control.endRefreshing()
+                                }
+                            }
+                            self.orderdetailtableview.reloadData()
+                        }
+                    }
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+        
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return goods.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "orderdetailCell", for: indexPath) as! OrderdetailTableViewCell
+        
+        // Configure the cell...
+        let good = goods[indexPath.row]
+        cell.name.text = good.name.description
+        cell.price.text = good.price.description
+        
+        
+        return cell
+    }
+    
+    
+    
+   
     /*
     // MARK: - Navigation
 
--- CP103D_Topic0308/Good.swift
+++ CP103D_Topic0308/Good.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+class Good: Codable {
+    var id: Int
+    var name: String
+    var descrip: String?
+    var price: Double
+    var mainclass: String
+    var subclass: String
+    var shelf: String //上架
+    var date:Date //日期
+    var evulation: Int //評價
+    init(id: Int, name: String, descrip: String, price: Double, mainclass: String, subclass: String, shelf: String, date: Date, evulation: Int) {
+        self.id = id
+        self.name = name
+        self.descrip = descrip
+        self.price = price
+        self.mainclass = mainclass
+        self.subclass = subclass
+        self.shelf = shelf
+        self.date = date
+        self.evulation = evulation
+    }
+}
