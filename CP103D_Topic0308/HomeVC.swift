0f3764c1e2393c04c648140db4b2124d6bc077dd
aceg224 <aceg224@gmail.com>
2019-04-05 14:14:00 +0800

    完成推播接收功能

--- CP103D_Topic0308/HomeVC.swift
+++ CP103D_Topic0308/HomeVC.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+import Starscream
 
 class HomeVC: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var mainclassSegment: UISegmentedControl!
     @IBOutlet weak var scrollView: UIScrollView!
-    
+    var socket: WebSocket!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let userInfo = loadUser()
+        let username = userInfo?.userName
+        let url_WebSocketserver = URL(string: wscommon_url + "websocketAll/" + username!)
+        socket = WebSocket(url: url_WebSocketserver!)
+        socket.delegate = self
+        socket.connect()
         // Do any additional setup after loading the view.
     }
     
@@ -50,3 +57,42 @@ class HomeVC: UIViewController, UIScrollViewDelegate {
     */
 
 }
+extension HomeVC: WebSocketDelegate{
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("websocket is connected")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("websocket is disconnected: \(error?.localizedDescription ?? "")")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        if let result = try? JSONDecoder().decode([String: String].self, from: text.data(using: .utf8)!) {
+            let textmessage = "\(result["userName"]!): \(result["message"]!)"
+            print(textmessage)
+            //這邊要彈出通知視窗
+            showCorrectAlert("\(result["message"]!)")
+        }
+        print("got some text: \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+//        print("\(self.tag) got some data: \(data.count)")
+    }
+    
+    func showCorrectAlert(_ inputmessage : String){
+        let correctAlert = UIAlertController(title: "新貨上架！！！", message: inputmessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Great!", style: .default) { (_) in
+            self.next()
+        }
+        correctAlert.addAction(okAction)
+        present(correctAlert, animated: true, completion: nil)
+    }
+
+    func next(){
+        let storyboard = UIStoryboard(name: "ConsumerHome", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "comsumerHome")
+        present(controller, animated: true, completion: nil)
+    }
+}
+
