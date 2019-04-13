9b258e7a6c029e7a3f2a7be01b20d2ecd6793122
pp055430 <tn00817411@gmail.com>
2019-04-13 14:55:20 +0800

    新增我的最愛（未完成）

--- CP103D_Topic0308/Favorite/FavoriteTVC.swift
+++ CP103D_Topic0308/Favorite/FavoriteTVC.swift
@@ -7,22 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
 class FavoriteTVC: UITableViewController {
 
+    var favorites = [Favorite]()
+    var context: NSManagedObjectContext!
+    
     @IBOutlet weak var nameLable: UILabel!
-    @IBOutlet weak var sizeLable: UILabel!
-    @IBOutlet weak var colorLable: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        tableView.rowHeight = 80
+        context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+        
+        let request = NSFetchRequest<Favorite>(entityName: "Favorite")
+        do {
+            favorites = try context.fetch(request)
+            tableView.reloadData()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    /* 設定可否移動資料列 */
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
 
-  
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
     
-    
+    func loadData(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<Favorite>(entityName: "Favorite")
+        do {
+            let favorites = try context.fetch(request)
+            for favorite in favorites {
+                imageView.image = UIImage(data: favorite.image!)
+                nameLable.text = favorite.name
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
