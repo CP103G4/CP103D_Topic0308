a46b1d1d5a21d3930f85e3da259ef97853163114
aceg224 <aceg224@gmail.com>
2019-04-15 15:40:41 +0800

    change imagesize to 512

--- CP103D_Topic0308/GooddetailViewController.swift
+++ CP103D_Topic0308/GooddetailViewController.swift
@@ -66,7 +66,8 @@ class GooddetailViewController: UIViewController {
             requestParam["param"] = "getImage"
             requestParam["id"] = goodDetail.id
             // 圖片寬度為tableViewCell的1/4，ImageView的寬度也建議在storyboard加上比例設定的constraint
-            requestParam["imageSize"] = UIScreen.main.bounds.width / 4
+//            requestParam["imageSize"] = UIScreen.main.bounds.width / 4
+            requestParam["imageSize"] = 512
             var image: UIImage?
             executeTask(url_server!, requestParam) { (data, response, error) in
                 if error == nil {
--- CP103D_Topic0308/ManagerGooddetailViewController.swift
+++ CP103D_Topic0308/ManagerGooddetailViewController.swift
@@ -29,7 +29,8 @@ class ManagerGooddetailViewController: UIViewController {
         requestParam["param"] = "getImage"
         requestParam["id"] = goodDetail.id
         // 圖片寬度為tableViewCell的1/4，ImageView的寬度也建議在storyboard加上比例設定的constraint
-        requestParam["imageSize"] = UIScreen.main.bounds.width / 4
+//        requestParam["imageSize"] = UIScreen.main.bounds.width / 4
+        requestParam["imageSize"] = 512
         var image: UIImage?
         executeTask(url_server!, requestParam) { (data, response, error) in
             if error == nil {
